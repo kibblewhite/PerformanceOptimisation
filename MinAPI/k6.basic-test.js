99a73764a4b397c7fc507c56527440bb65f9758b
Kibble White <kibblewhite@live.com>
2023-02-08 12:33:39 +0000

    A basic k6 load testing with a min api

---
+++ MinAPI/k6.basic-test.js
@@ -0,0 +1,8 @@
+﻿import http from "k6/http";
+
+export default function () {
+    let response = http.get("http://localhost:5263");
+    console.log(response.status);
+}
+
+// k6 run --vus 10 --duration 40s k6.basic-test.js
